8d9755c76f2761c1c2780ee5b983ee7f31618031
pescel <28l.pesce@gmail.com>
2017-04-16 17:41:23 -0600

    Register page styling

--- public/lib/components/Register.js
+++ public/lib/components/Register.js
@@ -21,18 +21,25 @@ export default class Register extends Component {
     const { addUser } = this.props
     const { newUserFirstName, newUserLastName, newUserEmail, newUserPassword, newUserName } = this.state
     return(
-      <div className='register-container'>
-        <form onSubmit={(e) => {
-          e.preventDefault()
-          addUser(newUserFirstName, newUserLastName, newUserEmail, newUserPassword)
-          this.setState({ newUserFirstName: '', newUserLastName: '', newUserEmail: '', newUserPassword: '' })
-        }}>
-          <input className='name-input' placeholder='New User First Name' onChange={this.updateInput} value={newUserFirstName} id="newUserFirstName" />
-          <input className='name-input' placeholder='New User Last Name' onChange={this.updateInput} value={newUserLastName} id="newUserLastName" />
-          <input className='email-input' placeholder='New User Email' onChange={this.updateInput} value={newUserEmail} id="newUserEmail"/>
-          <input className='password-input' placeholder='New User Password' onChange={this.updateInput} value={newUserPassword} id="newUserPassword" />
-            <Button className='new-user-btn' text='Add New User' />
-        </form>
+      <div>
+        <h2 className='register-title'>Sign up for AdoptFund:</h2>
+        <div className='register-container'>
+          <form onSubmit={(e) => {
+            e.preventDefault()
+            addUser(newUserFirstName, newUserLastName, newUserEmail, newUserPassword)
+            this.setState({ newUserFirstName: '', newUserLastName: '', newUserEmail: '', newUserPassword: '' })
+          }}>
+            <p className='new-user-text'>Your First Name:</p>
+            <input placeholder='First Name' onChange={this.updateInput} value={newUserFirstName} id='newUserFirstName' />
+            <p className='new-user-text'>Your Last Name:</p>
+            <input placeholder='Last Name' onChange={this.updateInput} value={newUserLastName} id='newUserLastName' />
+            <p className='new-user-text'>Your Email:</p>
+            <input placeholder='Email' onChange={this.updateInput} value={newUserEmail} id='newUserEmail' />
+            <p className='new-user-text'>Choose a Password:</p>
+            <input placeholder='Password' onChange={this.updateInput} value={newUserPassword} id='newUserPassword' />
+            <Button className='new-user-btn' text='Sign up!' />
+          </form>
+        </div>
       </div>
     )
   }
