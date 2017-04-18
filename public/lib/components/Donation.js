ecaa0798ca7f3ab0d915a6c36443b7046a6bba5e
chelle tuerk <michelletuerk@gmail.com>
2017-04-18 11:18:29 -0600

    removed console logs

--- public/lib/components/Donation.js
+++ public/lib/components/Donation.js
@@ -31,7 +31,6 @@ export default class Donation extends Component {
   render() {
     return (
       <section className='donate-section'>
-        {console.log('user', this.props.user)}
         <input placeholder='First Name' value={this.state.first} onChange={(e) => this.setState({first: e.target.value})}></input>
         <input placeholder='Last Name' value={this.state.last} onChange={(e) => this.setState({last: e.target.value})}></input>
         <input placeholder='Email' value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}></input>
