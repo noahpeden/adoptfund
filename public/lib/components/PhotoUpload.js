6f410197010920773f74635f3df059d8f6568790
Joshua Small <jksmall0631@gmail.com>
2017-05-14 19:22:25 -0600

    remove unnecessary function for image upload

--- public/lib/components/PhotoUpload.js
+++ public/lib/components/PhotoUpload.js
@@ -1,49 +1,30 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 
 class PhotoUpload extends Component {
-
   initUpload(e) {
     const files = document.getElementById('file-input').files
     const file = files[0]
+    let form = new FormData()
+    form.append('file', file)
     if (file == null) {
       return alert('No file  selected.')
     }
-    fetch(`https://f2ea21bf.ngrok.io/api/v1/family/pic?file-name=${file.name}&file-type=${file.type}`, {
+    fetch(`http://localhost:3000/api/v1/family/pic?file-name=${file.name}&file-type=${file.type}`, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ file }),
+      body: form,
     })
-    .then(data => {
-      data.json()
-    })
-    .then(data => {
-      this.uploadFile(file, data.signedRequest, data.url)
+    .then((response) => response.json())
+    .then((data) => {
+      return data
     })
     .catch(err => console.log(err))
   }
-
-  uploadFile(file, signedRequest, url) {
-    const xhr = new XMLHttpRequest()
-    xhr.open('PUT', signedRequest)
-    xhr.onreadystatechange = () => {
-     if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            console.log(xhr.status)
-          }
-          else {
-            alert('Could not upload file.')
-          }
-        }
-      }
-      xhr.send(file);
-    }
 
   render() {
     return (
       <div>
         <p id='status'>Select a family photo</p>
-        <input type="file" id="file-input" onChange={(e) => this.initUpload()} />
+        <input type='file' id='file-input' onChange={(e) => this.initUpload()} />
       </div>
     )
   }
